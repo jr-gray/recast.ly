0673a47477cefc43e023b16cbcbeb0962c9863cd
JR Gray <wgray@tuta.io>
2017-07-12 16:54:00 -0700

    Add VideoList, EntryList and player functionality

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,34 @@
-var App = () => (
-  <div>
-    <Nav />
-    <div className="col-md-7">
-      <VideoPlayer/>
-    </div>
-    <div className="col-md-5">
-      <VideoList/>
-    </div>
-  </div>
-);
+class App extends React.Component {
+	constructor() {
+		super();
+		this.onVideoClick = this.onVideoClick.bind(this);
+		this.state = {
+			vids: exampleVideoData[1]
+		};
+	}
+
+	onVideoClick(video) {
+		this.setState({
+			vids: video
+		});
+	}
+
+	render() {
+		return (
+      <div>
+		    <Nav />
+		    <div className="col-md-7">
+		      <VideoPlayer video={this.state.vids}/>
+		    </div>
+		    <div className="col-md-5">
+		      <VideoList videos={exampleVideoData} onVideoClick={this.onVideoClick} />
+		    </div>
+	 		</div>
+		);
+	}
+
+}
+
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
